ce624c9c6caf086a4e1940ae485f25a5536ddad4
sadiajuthi <sadia.5343@gmail.com>
2022-03-01 15:01:07 +0530

    commen added

--- play-card.js
+++ play-card.js
@@ -1,1 +1,13 @@
-console.log('conected');
+const loaddata = () => {
+    const searchInput = document.getElementById('search-input');
+    const error = document.getElementById('error');
+    const searchValue = searchInput.value;
+    if (isNaN(searchValue) || searchValue == '') {
+        // alert("Please enter a number"); or
+        error.innerText = 'please enter a number'
+    }
+    else {
+        console.log(searchValue);
+    }
+    searchInput.value = '';
+}
